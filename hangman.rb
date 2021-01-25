321c25f6029ea355a092464f558df2400d1d18b3
Nathan Lucy <cosmocoder1@gmail.com>
2021-01-25 11:31:47 -0600

    game intro dev

--- hangman.rb
+++ hangman.rb
@@ -1,34 +1,44 @@
 class Game
-  
-  def initialize()
-    @name = get_name()
-    @guesses = []
-    @secret_word = secret_word()
-    @progress = []
-  end
-  
-  def get_name()
-    puts "Please enter your name..."
-    self.name = gets.chomp
-  end
-
-  def intro ()
-    puts "Welcome to Hangman!"
-    sleep(2)
-    puts "Will you be..."
-    sleep(1)
-    puts "(1) Starting a new game..."
-    sleep(1)
-    puts "...or (2) continuing a saved game?"
-    game_type = gets.chomp
-    if game_type == 1
-      new_game()  
-    else
-      saved_game()
-    end  
-  end  
-
-  def new_game()
-
-
-end  
+  attr_accessor :name 
+ 
+   def initialize()
+    
+     @name = ""
+     @guesses = []
+     @secret_word = secret_word()
+     @progress = []
+   end
+   
+   def get_name()
+     puts "Please enter your name..."
+     @name = gets.chomp
+   end
+ 
+   def intro ()
+     puts "Welcome to Hangman!"
+     sleep(2)
+     puts "Will you be..."
+     sleep(1)
+     puts "(1) Starting a new game..."
+     sleep(1)
+     puts "...or (2) continuing a saved game?"
+     game_type = gets.chomp
+     if game_type == 1
+       new_game()  
+     else
+       saved_game()
+     end  
+   end  
+ 
+   def new_game()
+   end  
+ 
+   def secret_word()
+   end  
+ 
+ 
+ end  
+ 
+ newGame = Game.new
+ newGame.get_name()
+ puts newGame.name
