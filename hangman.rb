2cefe8962bdb918ea1d1af7270356818681978bc
Nathan Lucy <cosmocoder1@gmail.com>
2021-01-25 19:53:03 -0600

    new game logic dev - new word function

--- hangman.rb
+++ hangman.rb
@@ -7,6 +7,7 @@ class Game
      @guesses = []
      @secret_word = secret_word()
      @progress = []
+ 
    end
    
    def get_name()
@@ -14,31 +15,53 @@ class Game
      @name = gets.chomp
    end
  
-   def intro ()
-     puts "Welcome to Hangman!"
+   def intro()
+ 
+     puts "Welcome to Hangman, #{@name}!"
      sleep(2)
      puts "Will you be..."
      sleep(1)
      puts "(1) Starting a new game..."
      sleep(1)
      puts "...or (2) continuing a saved game?"
+ 
      game_type = gets.chomp
-     if game_type == 1
+ 
+     if game_type == "1"
        new_game()  
      else
        saved_game()
-     end  
+     end 
+ 
+   end  
+ 
+   def generate_word()
+     
+     word = File.readlines("dictionary.txt").sample.split('').pop().join('')
+     
+     puts word
+ 
    end  
  
    def new_game()
+ 
+     puts "Wonderful!  Choosing a random word now..."
+     sleep(2)
+     generate_word()
+     puts "...done!"
+ 
    end  
  
    def secret_word()
    end  
  
+   def run_new_game()
+     get_name()
+     intro()
+   end  
  
  end  
  
  newGame = Game.new
- newGame.get_name()
- puts newGame.name
+ newGame.run_new_game()
+ 
